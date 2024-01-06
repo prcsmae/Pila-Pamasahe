4edb94a7c044d604dc407f9565b918395db1cb5c
csjodes <jodi.gavano@gmail.com>
2024-01-06 15:06:07 +0800

    hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct fareInfo {
+	char startPoint[50];
+	char endPoint[50];
+	double baseFare;
+	double additional;
+};
 
 int main(){
-    printf("Hello World!");
-    // Jodi was here
+    struct FareInfo regularJeepney = {"Point A", "Point B", 13, 1.80}; // initialize variables
+    struct FareInfo airconJeepney = {"Point A", "Point B", 15, 1.80}; // initialize variables
+    
     return 0;
 }
